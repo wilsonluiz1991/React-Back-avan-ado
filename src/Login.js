a69d731b8c9950525bec6ac5856a10708670bf1d
wilsonluiz1991 <wilsonluiz31051991@gmail.com>
2021-02-14 17:10:39 -0300

    reactbackavanc

---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Login = (props) => {
+   return (
+       <div>
+           <h1>Login</h1>
+           <input>Digite seu E-mail</input>
+           <input>Digite sua senha</input>
+           <button>enviar</button>
+       </div>
+   )
+}
+
+export default Login;
